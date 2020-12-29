a330ae6c194a7e1c919209dd9630bf3445450d08
ramadhanriandi <izurbudianto@gmail.com>
2020-12-30 04:11:32 +0700

    add kafka producer

--- src/main/java/com/twitswap/kafkaproducer/KafkaProducerApplication.java
+++ src/main/java/com/twitswap/kafkaproducer/KafkaProducerApplication.java
@@ -1,13 +1,14 @@
 package com.twitswap.kafkaproducer;
 
-import org.springframework.boot.SpringApplication;
+import com.twitswap.kafkaproducer.producer.TwitterProducer;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaProducerApplication.class, args);
+//		SpringApplication.run(KafkaProducerApplication.class, args);
+		new TwitterProducer().run();
 	}
-
 }
